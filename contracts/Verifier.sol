16499bb3c05e821ac63eda425bd429e5e287245f
Drew Ripberger <drew.ripberger@gmail.com>
2023-01-01 23:35:39 -0500

    added poseidon from circomlibjs

--- contracts/Verifier.sol
+++ contracts/Verifier.sol
@@ -1,7 +1,18 @@
 pragma solidity ^0.8.17;
 import {SumCheck} from "./SumCheck.sol";
+import {Poseidon} from "./Poseidon.sol";
 
 contract Verifier {
+    Poseidon _poseidon;
+
+    constructor(address _poseidonContractAddr){
+        _poseidon = Poseidon(_poseidonContractAddr);
+    }
+
+    function hash2(uint256[2] memory inp) public view returns (uint256){
+        return _poseidon.hash2(inp);
+    }
+
     function test_eval_at_zero(uint[] memory g) public pure returns (uint) {
         return SumCheck.eval_at_zero(g);
     }
